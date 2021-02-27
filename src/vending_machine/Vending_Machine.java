b846f886268f7971cdd70bddf68c602880448b29
Richard Fehling <richard.fehling.mte21lin@tucsweden.se>
2021-02-27 13:14:57 +0100

    Merge origin/master
    Conflicts:
    	src/github1/GitHub1.java
    resolved

--- src/vending_machine/Vending_Machine.java
+++ src/vending_machine/Vending_Machine.java
@@ -13,39 +13,19 @@ public class Vending_Machine {
         Inventory.FillMerchList();
         MyWallet.AddToCredit();
         
-        //Creating dynamic menu with input 
+        //Start of main loop continue shopping or not
         int input = 0;
         do {            
-            boolean wrongNumber = false; //Setting boolean for continue shopping     
-        do {           
-            if(wrongNumber)System.out.println("Enter CORRECT number."); //Error if not a valid number or Exception
-            wrongNumber = false; //Setting boolean for first selection of item
-            System.out.println("Vending Machine");
-            System.out.println("_________________");
-           
-            //Make selection menu
-            MakeMenu(Inventory.merchList);
+            boolean wrongNumber = false; //Setting boolean for continue shopping
             
-            //Choice one item for description and purchase
-            System.out.print("Select one option for further details: ");
-            try {             
-                input = GetInput(); //Calling input function
-                System.out.println(""); //Just for cosmetics
-                if(1 > input || input > Inventory.merchList.size())wrongNumber = true;
-            }
-            catch(Exception e){
-            System.out.println("Enter a numeric value, try again"); //Error Exception
-            wrongNumber = true;
-            }           
-        } while (wrongNumber);
+            //Creating dynamic menu with input and select item
+            input = MakeMenuAndSelect();
         
+             //Merchandise description and add to cart
+            Inventory.merchList.get(input - 1).HeaderDescription(); //Header from abstract class inherit to all items
+            Inventory.DescriptionAndBuy(input);
         
-        //Merchandise description and add to cart
-        Inventory.merchList.get(input - 1).HeaderDescription(); //Header from abstract class inherit to all items
-        Inventory.DescriptionAndBuy(input);
-        
- 
-        //Continue shopping or checkout      
+            //Continue shopping or checkout      
         System.out.println("\n1 Continue shopping\n2 Check-out to collect items");
         
         do {
@@ -71,20 +51,53 @@ public class Vending_Machine {
        
         
     }
-        //Input function integer
+        //GetInput function integer
         public static int GetInput(){
-        Scanner scan = new Scanner(System.in);
-        int input = Integer.parseInt(scan.next());
-        return input;       
+            Scanner scan = new Scanner(System.in);
+            int input = Integer.parseInt(scan.next());
+            return input;       
         }
-
-        public static void MakeMenu(ArrayList merchList){
+        
+        //MakeMenuAndSelect function
+        public static int MakeMenuAndSelect(){
+            int input = 0;
+            boolean wrongNumber = false;
+            do {               
+                if(wrongNumber)System.out.println("Enter CORRECT number."); //Error if not a valid number or Exception
+                wrongNumber = false; //Setting boolean for first selection of item
+                System.out.println("Vending Machine");
+                System.out.println("_________________");
+           
+                //Make selection menu
+                MakeMerchMenu(Inventory.merchList);
+            
+                //Choice one item for description and purchase
+                System.out.print("Select one option for further details: ");
+                try {             
+                    input = GetInput(); //Calling input function
+                    System.out.println(""); //Just for cosmetics
+                    if(1 > input || input > Inventory.merchList.size())wrongNumber = true;
+                }
+                catch(Exception e){
+                System.out.println("Enter a numeric value, try again"); //Error Exception
+                wrongNumber = true;
+                }           
+            } while (wrongNumber); 
+            return input;
+        } 
+        
+        //MakeMerchMenu function dynamic menu 
+        public static void MakeMerchMenu(ArrayList merchList){
             int n = 1;
             for (MerchAbstract i : Inventory.merchList) {
                 System.out.println( n +" "+ i.name);
                 n++;}           
         }
-              
+        
+        //ContShoppingOrCheckout function
+        public static int ContShoppingOrCheckout(){
+            return ;
+        }
 }
        
       
