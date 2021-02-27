3c4da0cd38c1281de5f97c2680d2a8d4b5c8269a
Richard Fehling <richard.fehling.mte21lin@tucsweden.se>
2021-02-27 14:28:14 +0100

    Merge origin/master
    Conflicts:
    	src/github1/GitHub1.java
    resolved

--- src/vending_machine/Vending_Machine.java
+++ src/vending_machine/Vending_Machine.java
@@ -9,48 +9,29 @@ public class Vending_Machine {
 
     public static void main(String[] args) {
     
-        //Fill upp merchList from Inventory
+        //Fill upp merchList from Inventory and fill upp kredit
         Inventory.FillMerchList();
         MyWallet.AddToCredit();
         
         //Start of main loop continue shopping or not
-        int input = 0;
-        do {            
-            boolean wrongNumber = false; //Setting boolean for continue shopping
-            
+        int input2;
+        do {                      
             //Creating dynamic menu with input and select item
-            input = MakeMenuAndSelect();
+            int input1 = MakeMenuAndSelect();
         
-             //Merchandise description and add to cart
-            Inventory.merchList.get(input - 1).HeaderDescription(); //Header from abstract class inherit to all items
-            Inventory.DescriptionAndBuy(input);
+            //Merchandise description and add to cart
+            Inventory.merchList.get(input1 - 1).HeaderDescription(); //Header from abstract class inherit to all items
+            Inventory.DescriptionAndBuy(input1);
         
-            //Continue shopping or checkout      
-        System.out.println("\n1 Continue shopping\n2 Check-out to collect items");
-        
-        do {
-            
-            if(wrongNumber)System.out.println("Enter CORRECT number."); //Error if not a valid number or Exception
-            wrongNumber = false;
-            try {
-                System.out.print("Make your choice: ");
-                input = GetInput();
-                System.out.println("");
-                if(1 > input || input > 2)wrongNumber = true;
-            }
-            catch(Exception e){
-            System.out.println("Enter a numeric value, try again"); //Error Exception
-            wrongNumber = true;
-            }           
-        } while (wrongNumber);           
-        } while (input == 1);
+            //Continue shopping or checkout
+            input2 = ContShoppingOrCheckout();
 
-        //Shopping cart
-      
-        MyWallet.Balance();
-       
-        
+        } while (input2 == 1);
+
+        //Shopping cart    
+        MyWallet.Balance();     
     }
+    
         //GetInput function integer
         public static int GetInput(){
             Scanner scan = new Scanner(System.in);
@@ -65,7 +46,7 @@ public class Vending_Machine {
             do {               
                 if(wrongNumber)System.out.println("Enter CORRECT number."); //Error if not a valid number or Exception
                 wrongNumber = false; //Setting boolean for first selection of item
-                System.out.println("Vending Machine");
+                System.out.println("\nVending Machine");
                 System.out.println("_________________");
            
                 //Make selection menu
@@ -96,7 +77,24 @@ public class Vending_Machine {
         
         //ContShoppingOrCheckout function
         public static int ContShoppingOrCheckout(){
-            return ;
+            int input = 0;
+            boolean wrongNumber = false;
+            System.out.println("\n1 Continue shopping\n2 Check-out to collect items");       
+            do {         
+                if(wrongNumber)System.out.println("Enter CORRECT number."); //Error if not a valid number or Exception
+                wrongNumber = false;
+                try {
+                    System.out.print("Make your choice: ");
+                    input = GetInput();
+                    System.out.println("");
+                    if(1 > input || input > 2)wrongNumber = true;
+                }
+            catch(Exception e){
+            System.out.println("Enter a numeric value, try again"); //Error Exception
+            wrongNumber = true;
+            }           
+            } while (wrongNumber); 
+            return input;
         }
 }
        
